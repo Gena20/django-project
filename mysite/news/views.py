e44feacf4d57bc74e469ae414ebc896ae04d3449
Gena <peryatin.gena@yandex.ru>
2021-06-19 13:52:00 +0500

    Added rubrics output

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from news.models import Article, Rubric, Hashtag
+
 
 def index(request):
-    return render(request, 'index.html', {})
+    context = make_default_context()
+    return render(request, 'index.html', context)
 
 
 def article(request):
-    return render(request, 'article.html', {})
+    context = make_default_context()
+    return render(request, 'article.html', context)
+
+
+def make_default_context(context={}):
+    return {'rubrics': Rubric.objects.all()} | context
